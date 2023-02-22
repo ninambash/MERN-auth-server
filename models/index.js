6f8f6257154c0873e866ca4d299af956c639d925
ninambash <ninambash@gmail.com>
2023-02-22 15:21:54 -0800

    updated

--- models/index.js
+++ models/index.js
@@ -1,28 +1,26 @@
-const mongoose = require("mongoose")
-require('dotenv').config()
+const mongoose = require('mongoose');
+require('dotenv').config();
 
-
-const dbName = 'mernAuth'
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://127.0.0.1/' + dbName
+const dbName = 'mernAuth';
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://127.0.0.1/' + dbName;
 
 mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
-  useUnifiedTopology: true
-})
+  useUnifiedTopology: true,
+});
 
-const db = mongoose.connection
+const db = mongoose.connection;
 
 // Connection methods
 db.once('open', () => {
-  console.log(`🔗 Connected to MongoDB at ${db.host}:${db.port}`)
-})
+  console.log(`🔗 Connected to MongoDB at ${db.host}:${db.port}`);
+});
 
-db.on('error',  err => {
-  console.error(`🔥 Datacenter burned down:\n${err}`)
-})
-
+db.on('error', (err) => {
+  console.error(`🔥 Datacenter burned down:\n${err}`);
+});
 
 module.exports = {
   User: require('./User'),
-  Campaign: require('./Campaign')
-}
+  Campaign: require('./Campaign'),
+};
