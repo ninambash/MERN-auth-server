244c0a6a5dcac39320627b570104d3ae43259655
ninambash <ninambash@gmail.com>
2023-02-22 16:18:58 -0800

    updated

--- controllers/api-v1/users.js
+++ controllers/api-v1/users.js
@@ -1,5 +1,6 @@
 const router = require('express').Router()
 const db = require('../../models')
+
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const authLockedRoute = require('./authLockedRoute')
--- controllers/campaign.js
+++ controllers/campaign.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { isValidObjectId } = require('mongoose');
+//const { isValidObjectId } = require('mongoose');
 //const  Campaign  = require('../models');
 const router = express.Router();
 const db = require('../models');
